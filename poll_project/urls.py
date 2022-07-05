a5f676c9a02cf4af70e2eafa32e161474df02707
Moses Karani <karanim594@gmail.com>
2022-07-05 11:48:46 +0300

    optimized base and updated urls

--- poll_project/urls.py
+++ poll_project/urls.py
@@ -13,11 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from poll import views
+from django.conf import settings
+from django.conf.urls.static import static
+from poll import views as poll_views
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-]
+    path('', poll_views.index, name='index'),
+    path('create/', poll_views.create, name='create'),
+    path('vote/<poll_id>/', poll_views.vote, name='vote'),
+    path('results/<poll_id>/', poll_views.results, name='results'),
+] 
+if settings.DEBUG:
+ urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
