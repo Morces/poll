28c3679317f837ed1e88277011c57c6677af4ffd
Moses Karani <karanim594@gmail.com>
2022-07-05 08:36:59 +0300

    made 1st migration and created index view template

--- poll/models.py
+++ poll/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Poll(models.Model):
+    question = models.TextField()
+    choice1 = models.CharField(max_length=40)
+    choice2 = models.CharField(max_length=40)
+    choice3 = models.CharField(max_length=40)
+    choice1_count = models.IntegerField(default=0)
+    choice2_count = models.IntegerField(default=0)
+    choice3_count = models.IntegerField(default=0)
