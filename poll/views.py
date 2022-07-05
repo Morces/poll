05d1df8db3a51f431260d025aaecab52d394385f
Moses Karani <karanim594@gmail.com>
2022-07-05 11:01:29 +0300

    created other templates

--- poll/views.py
+++ poll/views.py
@@ -2,4 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render('index.html')
+    return render(request, 'index.html')
+
+def create(request):
+    params = {}
+    return render(request, 'create.html', params)
+
+def vote(request, poll_id):
+    params = {}
+    return render(request, 'vote.html',params)
+
+def results(request, poll_id):
+    params = {}
+    return render(request, 'results.html',params)
