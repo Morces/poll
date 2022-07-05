9ac3cebfcad7d283b2eeeef10935dd64feb0be48
Moses Karani <karanim594@gmail.com>
2022-07-05 13:25:48 +0300

    optimized create template using forms

---
+++ poll/forms.py
@@ -0,0 +1,7 @@
+from django.forms import ModelForm
+from .models import Poll
+
+class CreatePollForm(ModelForm):
+    class Meta:
+        model = Poll
+        fields = ('question', 'option1', 'option2', 'option3', 'option4')
