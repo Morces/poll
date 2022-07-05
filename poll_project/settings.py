415ed4d50b914037fbd96098e8ac7ada9c892726
Moses Karani <karanim594@gmail.com>
2022-07-05 08:27:33 +0300

    created pstgresql and configured it

--- poll_project/settings.py
+++ poll_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'poll',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,10 @@ WSGI_APPLICATION = 'poll_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'poll',
+        'USER': 'moringa',
+        'PASSWORD': 'morces',
     }
 }
 
